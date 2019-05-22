38fec6d37f1a69375b1e114ca23ae51894f9c812
Judzhin Miles <info@msbios.com>
2019-05-22 12:18:54 +0300

    Lazy Composer MSBios Commit Script

--- config/module.config.php
+++ config/module.config.php
@@ -6,8 +6,38 @@
 
 namespace MSBios\Log;
 
+use Zend\Log\Formatter\Simple;
+use Zend\Log\Logger;
+
 return [
     'logs' => [
         'enabled' => false,
+        'MSBios\Logger' => [
+            'writers' => [
+                [
+                    'name' => 'stream',
+                    'priority' => Logger::INFO,
+                    'options' => [
+                        'stream' => './data/logs/logger.log',
+                        'formatter' => [
+                            'name' => Simple::class,
+                            'options' => [
+                                'format' => '%timestamp% %priorityName% (%priority%): %message% %extra%',
+                                'dateTimeFormat' => 'c',
+                            ],
+                        ],
+                        'filters' => [
+                            'priority' => [
+                                'name' => 'priority',
+                                'options' => [
+                                    'operator' => '<=',
+                                    'priority' => Logger::INFO,
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
     ],
 ];
